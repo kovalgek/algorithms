91241b0103771a3ac0823f83cc02a274ad4c2acc
Anton Kovalchuk <koval400k@gmail.com>
2019-11-25 22:01:03 +0100

    linera search

---
+++ LinearSearch/main.py
@@ -0,0 +1,30 @@
+
+def linearSearch(A, x):
+
+    left = 0
+    right = len(A) - 1
+    mid = 0
+
+    while True:
+
+        mid = (left + right) // 2
+
+        if x > A[mid]:
+            mid = mid + 1
+            left = mid
+        if x < A[mid]:
+            mid = mid - 1
+            right = mid
+
+        if A[mid] == x or left >= right:
+            break
+
+    return mid
+
+A = [1, 4, 6, 8, 15, 35, 67, 199, 245, 567, 789, 1000]
+
+mid = linearSearch(A, 1000)
+
+print(mid)
+
+
